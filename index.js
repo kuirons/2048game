c5a0f8dc0fa31f71f6512c9e4f74575a20bd719e
kuirons <wugyu@outlook.com>
2017-06-10 14:54:32 +0800

    change:随机生成数字位置

--- index.js
+++ index.js
@@ -5,11 +5,11 @@
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});
 document.body.appendChild(app.view);
 
-var basicText = new PIXI.Text('2048game', {fontSize: 120});
-basicText.anchor.set(0.5)
-basicText.x = window.innerWidth / 2;
-basicText.y = window.innerHeight / 4.5;
-app.stage.addChild(basicText);
+var number = new PIXI.Text('2048game', {fontSize: 120});
+number.anchor.set(0.5)
+number.x = window.innerWidth / 2;
+number.y = window.innerHeight / 4.5;
+app.stage.addChild(number);
 
 var grid = []
 for (var i = 0; i < 4; i++) {
@@ -22,5 +22,23 @@ for (var i = 0; i < 4; i++) {
         graphics.drawRoundedRect((window.innerWidth-165*4) / 2+j*165, (window.innerHeight-165*4) / 1.8+i*165, 160, 160, 15);
         app.stage.addChild(graphics)
     }
-
 }
+
+function generateRandom() {
+    return Math.floor(Math.random()*4)
+}
+
+var x=generateRandom()
+var y=generateRandom()
+
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xFF0033, 0.25);
+graphics.drawRoundedRect((window.innerWidth - 165 * 4) / 2 + x * 165, (window.innerHeight - 165 * 4) / 1.8 + y * 165, 160, 160, 15);
+app.stage.addChild(graphics)
+
+var number = new PIXI.Text('2', {fontSize: 100});
+number.anchor.set(0.5)
+number.x = (window.innerWidth-165*4) / 2+x*165+165/2;
+number.y = (window.innerHeight-165*4) / 1.8+y*165+165/2;
+app.stage.addChild(number);
+
